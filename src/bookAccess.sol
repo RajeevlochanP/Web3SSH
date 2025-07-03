c2a52735bcde00819837040564c43f1defc1cc59
unknown <rajeevlochanperla@gmail.com>
2025-07-04 02:17:46 +0530

    2 contracts completed

---
+++ src/bookAccess.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract BookAccess1155 is ERC1155, Ownable {
+    uint public nextTokenId;
+
+    IERC20 public coins;
+
+    constructor(address _tokenAddress) ERC1155("BookId") Ownable(msg.sender) {
+        coins = IERC20(_tokenAddress);
+    }
+
+    struct BookMetaData {
+        string name;
+        string description;
+        address author;
+        uint price;
+    }
+
+    mapping(uint => string) public tokenURIs;   // tokenId →  CID
+    mapping(uint => BookMetaData) public bookMetaDatas; // tokenId → metadata
+    mapping(address => uint[]) public booksByAuthor; //books by author → tokenIds
+
+    function upload(string memory _cid,string memory _name,string memory _desc,uint _price) external returns (uint tokenId) {
+        tokenId = nextTokenId++;
+        tokenURIs[tokenId] = _cid;
+
+        bookMetaDatas[tokenId] = BookMetaData({
+            name: _name,
+            description: _desc,
+            author: msg.sender,
+            price: _price
+        });
+        booksByAuthor[msg.sender].push(tokenId);
+        _mint(msg.sender, tokenId,1, "");
+    }
+
+    function buyBook(uint tokenId) external {
+        require(bytes(tokenURIs[tokenId]).length != 0, "Book does not exist");
+        BookMetaData memory book = bookMetaDatas[tokenId];
+        require(coins.balanceOf(msg.sender) >= book.price, "Insufficient token balance");
+        require(coins.transferFrom(msg.sender, book.author, book.price),"Payment failed");
+        _mint(msg.sender, tokenId, 1, "");
+    }
+
+    function getBookURI(uint tokenId) external view returns (string memory) {
+        require(balanceOf(msg.sender, tokenId) > 0, "Access denied: Buy the book first");
+        return string(abi.encodePacked("ipfs://", tokenURIs[tokenId]));
+    }
+}
---
+++ src/coin.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract coins is ERC20, Ownable {
+
+    // uint256 public constant INITIAL_SUPPLY = 1e27;
+    uint256 public constant weii=3; //for adding coins
+    uint256 public constant weif=4; // for withdrawing 
+    uint256 public constant MINIMUM_STAKE_VALUE=1000;
+    mapping(address => uint256)stakedAmounts;
+
+    event coinsBought(address recipient, uint256 amount);
+    event withdrawn(address recipient, uint256 amount);
+    event staked(address user, uint256 amount);
+    event unStaked(address user, uint256 amount);
+
+    constructor() ERC20("COIN", "COIN") Ownable(msg.sender) {
+        console.log(address(this));
+    }
+
+    function getCoins() external payable {
+        uint amount=msg.value*weii;
+        // require(balanceOf(address(this))>=amount,"Insufficient amount in contract");
+        _mint(msg.sender, amount);
+        emit coinsBought(msg.sender,amount);
+    }
+
+    function withdrawCoin(uint noOfCoins) external {
+        require(balanceOf(msg.sender) >= noOfCoins, "Insufficient tokens");
+        require(noOfCoins%weif==0,"only divisible by weif can be withdrawn");
+        uint amount =noOfCoins/weif;
+        require(address(this).balance >= amount, "insufficient Ether in contract");
+        _burn(msg.sender, noOfCoins);
+        payable(msg.sender).transfer(amount);
+        emit withdrawn(msg.sender,amount);
+    }
+
+    //ragavan use these functions for node operators staking
+    function stake(uint amount) external{
+        require(amount>=MINIMUM_STAKE_VALUE,"minimum stake value");
+        _transfer(msg.sender,address(this) ,amount);
+        stakedAmounts[msg.sender]+=amount;
+        emit staked(msg.sender, amount);
+    }
+
+    function unStake() external{
+        // require(amount>=MINIMUM_STAKE_VALUE,"minimum stake value is 1000");
+        _transfer(address(this),msg.sender,stakedAmounts[msg.sender]);
+        emit unStaked(msg.sender, stakedAmounts[msg.sender]);
+        stakedAmounts[msg.sender]=0;
+    }
+    
+    fallback() external payable {}
+    receive() external payable {}
+}
