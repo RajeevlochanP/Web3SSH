9de154fa64cc78441063e1c586773c8576dc5ad2
Muzammil363 <sk3151333@gmail.com>
2025-07-04 16:34:20 +0530

    Added Purchase coins

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,16 +2,23 @@ import styles from '../styles/Header.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { connectionActions } from '../store/store.js'
 import toast from 'react-hot-toast';
+import { useState } from 'react';
 
 function Header() {
     const dispatch = useDispatch();
     const isConnected=useSelector(state=>state.connect.isConnected);
+    const [openBuy,setOpenBuy]=useState(false);
 
     function handleConnect() {
         // connecting to wallet logic goes here
         dispatch(connectionActions.connect());
         toast.success("Connected to wallet successfully");
     }
+
+    function handlePurchase() {
+        //
+    }
+
     return (
         <header className={styles.header}>
             <div className={styles.container}>
@@ -23,10 +30,44 @@ function Header() {
                     <a href="#home" className={styles.navLink}>Home</a>
                     <a href="#features" className={styles.navLink}>Features</a>
                     <a href="#about" className={styles.navLink}>About</a>
-                    <a href="#contact" className={styles.navLink}>Contact</a>
                 </nav>
                 <div className={styles.actions}>
-                   {!isConnected && <button className={styles.signupBtn} onClick={handleConnect}>Connect Wallet</button>}
+                   {!isConnected && 
+                   <button 
+                        className={styles.signupBtn} 
+                        onClick={handleConnect}
+                   >
+                    Connect Wallet</button>}
+
+                   {(isConnected && !openBuy) &&
+                   <button 
+                        className={styles.signupBtn}
+                        onClick={()=>{setOpenBuy(true)}}
+                    >
+                    Purchase V</button>}
+
+                    {openBuy && 
+                        <div className={styles.actions}> 
+                            <input 
+                                type="text" 
+                                placeholder='Enter Number of V'
+                                className={styles.searchInput}
+                            />
+                            <button 
+                                className={styles.signupBtn} 
+                                onClick={handlePurchase}
+                            >Buy</button>
+
+                            <button 
+                                className={styles.loginBtn} 
+                                onClick={()=>{setOpenBuy(false)}}
+                            >Cancel</button>
+                        </div>
+                    }
+
+                    <div className={styles.actions}>
+                        
+                    </div>
                 </div>
             </div>
         </header>
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore,createSlice } from "@reduxjs/toolkit";
+import { data } from "react-router-dom";
 
 
 const connectionSlice=createSlice({
@@ -10,6 +11,19 @@ const connectionSlice=createSlice({
         },
         disconnect(state) {
             state.isConnected=false;
+        }
+    }
+});
+
+const userSlice=createSlice({
+    name:'usedata',
+    initialState:{data:{item1:'',item2:'',item3:''}},
+    reducers:{
+        setData(state,action) {
+            const {item1,item2,item3}=action.payload
+            state.data.item1=item1
+            state.data.item2=item2
+            state.data.item3=item3
         }
     }
 });
